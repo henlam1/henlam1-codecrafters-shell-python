e5f15d9ee6d12ef01e80093989b5116414e51b4c
henlam <henry.lam.1@stonybrook.edu>
2024-06-22 06:25:04 -0400

    hard code exit code 0

--- app/main.py
+++ app/main.py
@@ -9,13 +9,14 @@ def main():
     # Wait for user input
     args = input().split(" ")
     cmd = args[0]
+    cmd_arg = ' '.join(args[1:])
 
     # Exit command
     if cmd == "exit":
-        exit(args[1])
+        exit(0)
     # Echo command
     elif cmd == "echo":
-        print(" ".join(args[1:]))
+        print(cmd_arg)
     # Handle missing commands
     else:
         sys.stdout.write(f"{cmd}: command not found\n")
